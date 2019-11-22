d808cfe50ee249db948aeb5c368d1ccbd0e86d2b
chrisguy <chris@chrisguy.co>
2019-11-22 11:45:24 +0000

    config added. user resource (wip)

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 app.register_blueprint(courses_api)
 app.register_blueprint(reviews_api, url_prefix='/api/v1')
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
---
+++ config.py
@@ -0,0 +1,2 @@
+
+SECRET_KEY = 'hhdide<.83bjhsh2{&@@^Jnnwjklws;--09hdwjkhkjw'
